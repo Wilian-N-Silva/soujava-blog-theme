bcdc4fa11b343b8562d19960e2ee12b39fa1df69
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-04-19 12:47:04 -0300

    feat: :sparkles: Listen to window theme preferences

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,40 @@
+const DARK_THEME_PREFERENCE_KEY = "prefersDarkTheme"
+
 const themeBtn = document.getElementById("change-theme-btn")
 const body = document.querySelector("body")
 
+const changeTheme = (state) => {}
+
+const handleDarkTheme = () => {
+  const prefersDarkTheme = Number(
+    localStorage.getItem(DARK_THEME_PREFERENCE_KEY)
+  )
+
+  const bodyClasses = body.classList
+
+  const bodyContainsDarkClass = bodyClasses.contains("dark")
+
+  if (prefersDarkTheme && !bodyContainsDarkClass) {
+    bodyClasses.add("dark")
+    return
+  }
+
+  if (!prefersDarkTheme && bodyContainsDarkClass) {
+    bodyClasses.remove("dark")
+    return
+  }
+}
+
 themeBtn.addEventListener("click", function (ev) {
-  body.classList.toggle("dark")
+  let isDarkPrefer = Number(localStorage.getItem(DARK_THEME_PREFERENCE_KEY))
+  localStorage.setItem(DARK_THEME_PREFERENCE_KEY, Boolean(isDarkPrefer) ? 0 : 1)
+  handleDarkTheme()
 })
+
+window.onload = () => {
+  if (!localStorage.getItem(DARK_THEME_PREFERENCE_KEY)) {
+    let isDarkPrefer = window.matchMedia("(prefers-color-scheme: dark)").matches
+    localStorage.setItem(DARK_THEME_PREFERENCE_KEY, isDarkPrefer ? 1 : 0)
+  }
+  handleDarkTheme()
+}
